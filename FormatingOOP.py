de16abc70ac416be96a39e2c8809d4e3db92879c
ahmaddroobi99 <ahmad.droobi1999@gmail.com>
2021-07-01 10:58:55 +0300

    commitedCode

--- FormatingOOP.py
+++ FormatingOOP.py
@@ -1,4 +1,4 @@
-class Person():
+class Person:
     def __init__(self, first_name, last_name, ide, nationality):
         self.first_name = first_name
         self.last_name = last_name
@@ -7,15 +7,11 @@ class Person():
 
     def print_person(self):
         print(
-            f'hi my name is {self.first_name} {self.last_name} and my nationality is  {self.nationality}and my ide '
-            f'in case u wanna contact  {self.ide}',
+            f"hi my name is {self.first_name} {self.last_name} and my nationality is  {self.nationality}and my ide "
+            f"in case u wanna contact  {self.ide}",
         )
 
 
-user1 = Person('ahmad ', 'Droobi', '4047297855567', 'Palestinian')
+user1 = Person("ahmad ", "Droobi", "4047297855567", "Palestinian")
 
 user1.print_person()
-
-
-
-
---
+++ formatingwithBlackTool.py
@@ -0,0 +1,14 @@
+import FormatingOOP
+
+
+print("hi iam formating with black tool based on PEP8")
+
+x = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+firstName = "ahmad"
+LastName = "droobi"
+Nationality = "palestinian"
+ide = "404729785854852565"
+
+
+s = FormatingOOP.Person(firstName, LastName, Nationality, ide)
+s.print_person()
